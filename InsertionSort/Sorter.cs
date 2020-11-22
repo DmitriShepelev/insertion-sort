d52e9d55544925645d5c90b5bcde1b399589a7df
Дмитрий Шепелев <shepelev.dm@gmail.com>
2020-11-22 21:47:19 +0300

    ver. 1

--- InsertionSort/Sorter.cs
+++ InsertionSort/Sorter.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 // ReSharper disable InconsistentNaming
 #pragma warning disable SA1611
@@ -12,8 +12,25 @@ namespace InsertionSort
         /// </summary>
         public static void InsertionSort(this int[] array)
         {
-            // TODO #1. Implement the method using a loop statements.
-            throw new NotImplementedException();
+            // #1. Implement the method using a loop statements.
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+
+            for (int j = 1; j < array.Length; j++)
+            {
+                var key = array[j];
+                var i = j - 1;
+                while (i >= 0 && array[i] > key)
+                {
+                    array[i + 1] = array[i];
+                    i--;
+                }
+
+                array[i + 1] = key;
+            }
         }
 
         /// <summary>
@@ -21,8 +38,34 @@ namespace InsertionSort
         /// </summary>
         public static void RecursiveInsertionSort(this int[] array)
         {
-            // TODO #2. Implement the method using recursion algorithm.
-            throw new NotImplementedException();
+            // #2. Implement the method using recursion algorithm.
+            if (array is null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            static void Sort(int[] arr, int length)
+            {
+                if (length <= 1)
+                {
+                    return;
+                }
+
+                Sort(arr, length - 1);
+
+                var key = arr[length - 1];
+                var i = length - 2;
+
+                while (i >= 0 && arr[i] > key)
+                {
+                    arr[i + 1] = arr[i];
+                    i--;
+                }
+
+                arr[i + 1] = key;
+            }
+
+            Sort(array, array.Length);
         }
     }
 }
